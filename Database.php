acd268d21fa96f9aa0056d842178cb4a3190fbda
“HernandoMoreno” <hernandomoreno@hotmail.com>
2025-11-23 16:34:33 -0500

    Avnace 23nov25 4:34

---
+++ Database.php
@@ -0,0 +1,62 @@
+<?php
+class Database {
+    //Atributos
+    private $host = "localhost";
+    private $db_name = "inventario";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    //Establecer la conexión con la bd
+    public function conectar() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                $this->username, 
+                $this->password
+            ); //Establece la conexión
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Maneja los errores
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    // MÉTODO PARA BUSCAR USUARIO (LOGIN)
+    public function buscarUsuario($usuario, $password) {
+        try {
+            $sql = "SELECT * FROM usuarios WHERE usuario = ? AND password = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$usuario, $password]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    // MÉTODO PARA AGREGAR USUARIO (REGISTRO)
+    public function agregarUsuario($usuario, $password, $nombre, $apellidos, $celular) {
+        try {
+            $sql = "INSERT INTO usuarios (usuario, password, nombre, apellidos, celular) VALUES (?, ?, ?, ?, ?)";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$usuario, $password, $nombre, $apellidos, $celular]);
+        return true;
+        } catch (PDOException $e) { //PDO es la forma moderna de conectarse a bd en PHP.
+            echo "Error al agregar usuario: " . $e->getMessage();
+            return false;
+    }
+
+    // MÉTODO PARA VERIFICAR SI USUARIO EXISTE PARA CREARLO
+    public function usuarioExiste($usuario) {
+        try {
+            $sql = "SELECT id FROM usuarios WHERE usuario = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$usuario]);
+            return $stmt->fetch(PDO::FETCH_ASSOC) !== false;
+        } catch (PDOException $e) { //PDO es la forma moderna de conectarse a bd en PHP.
+            return false;
+        }
+    }
+}
+?>
